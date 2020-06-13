256ba6d8a102ce0aa7e9b48697a5a26a579d2077
Eze Sunday Eze <mailsforeze@gmail.com>
2020-06-13 15:48:11 +0100

    completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import jwt
 import stream
 import os
@@ -15,11 +16,17 @@ load_dotenv()
 STREAM_SECRET_KEY = os.getenv("STREAM_SECRET_KEY")
 STREAM_API_KEY = os.getenv("STREAM_API_KEY")
 
+
 client = stream.connect(STREAM_API_KEY, STREAM_SECRET_KEY, location='us-east')
+
+CORS(app, origins="*", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
 
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -35,7 +42,7 @@ class Shipment(db.Model):
     status = db.Column(db.String(120))
     item = db.Column(db.String(120))
     description = db.Column(db.String(120))
-    tracking_number = db.Column(db.String(120))
+    tracking_number = db.Column(db.String(120), nullable=True)
     arrival = db.Column(db.String(120))
 
     def __repr__(self):
@@ -52,7 +59,6 @@ class Shipment(db.Model):
         self.tracking_number = tracking_number
         self.arrival = arrival
 
-
 class ShipmentSchema(ma.Schema):
     """
     Schema
@@ -64,6 +70,7 @@ class ShipmentSchema(ma.Schema):
         'description', 
         'status', 
         'tracking_number',
+        'current_location',
         'source',
         'destination',
         'description',
@@ -92,35 +99,32 @@ class ShipmentView(Resource):
     """
     Shipment management API
     """
-    parser = reqparse.RequestParser()
-    parser.add_argument('description', type=str)
-    parser.add_argument('destination', type=str)
-    parser.add_argument('source', type=str)
-    parser.add_argument('status', type=str)
-    parser.add_argument('arrival', type=str)
-    parser.add_argument('current_location', type=str)
-    parser.add_argument('tracking_number', type=str)
-    parser.add_argument('item', type=str)
-    parser.add_argument('Authorization', location='headers', type=str)
 
-    def get(self):
+    def get(self, id=None):
         """
         get Shipment
         """
         try:
-            shipment = Shipment.query.filter().all()
-            shipment_schema =  ShipmentSchema(many=True)
-            return shipment_schema.dump(shipment)
+            if id is None:
+                shipment = Shipment.query.filter().all()
+                shipment_schema =  ShipmentSchema(many=True)
+                return shipment_schema.dump(shipment)
+            else:
+                shipment = Shipment.query.filter_by(id=id).first()
+                shipment_schema = ShipmentSchema()
+                return shipment_schema.dump(shipment)
+
         except Exception as e:
             print(e)
 
     def post(self):
+    
         """
         Add shipment
         """
-        data = self.parser.parse_args()
+        data = request.get_json()
 
-        token = data["Authorization"]     
+        token = data['headers']['Authorization']   
 
         # decode token
         user = jwt.decode(token[7:], STREAM_SECRET_KEY)
@@ -128,16 +132,16 @@ class ShipmentView(Resource):
         # Instantiate a feed object
         user_feed = client.feed('user', user['user_id'])
 
-
         try:
-            del data['Authorization']
+            del data['headers']
             new_shipment = Shipment(**data)
-
             db.session.add(new_shipment)
             db.session.commit()
 
-            # Add an activity to the feed, where actor, object and target are references to objects (`Eric`, `Hawaii`, `Places to Visit`)
-
+            """ 
+            Add an activity to the feed, where actor, 
+            object and target are references to objects (`Eric`, `Hawaii`, `Places to Visit`)
+            """
             activity_data = {
             "actor": user['user_id'] , 
             "verb": "ship", 
@@ -162,42 +166,39 @@ class ShipmentView(Resource):
         Update shipment
         """
         try:
-            data = self.parser.parse_args()
-            token = data["Authorization"]
-
-            new_data = {}
-
-            for item, value in data.items():
-                if value is not None:
-                    new_data.update({item: value})
-
-            # decode token
-            user = jwt.decode(token[7:], STREAM_SECRET_KEY)
-
-            # Instantiate a feed object
-            user_feed = client.feed('user', user['user_id'])
-
-            del new_data['Authorization']
-
-            shipment = Shipment.query.filter_by(id=id)
-            shipment.update(new_data)
-            db.session.commit()
             
-            activity_data = {
-            "actor": user['user_id'] , 
-            "verb": "ship", 
-            "object": "Place:42",
-            }
+            data = request.get_json()
 
-            activity_data.update(new_data)
+            if data.get("headers").get("Authorization"):
+            
+                token = data['headers']['Authorization']   
+                
+                # decode token
+                user = jwt.decode(token[7:], STREAM_SECRET_KEY)
 
-            user_feed.add_activity(activity_data)
+                # Instantiate a feed object
+                user_feed = client.feed('user', user['user_id'])
+
+                del data['headers']
+
+                shipment = Shipment.query.filter_by(id=id)
+                shipment.update(data)
+                db.session.commit()
+                
+                activity_data = {
+                "actor": user['user_id'] , 
+                "verb": "ship", 
+                "object": "Place:42",
+                }
+
+                activity_data.update(data)
+
+                user_feed.add_activity(activity_data)
+                return jsonify(data)
 
         except Exception as e:
-
             print(e)
-
-        return jsonify(new_data)
+            return { "message": "Error updating shipment"}
 
 # Routes
 api.add_resource(GenerateToken, '/generate-token')
